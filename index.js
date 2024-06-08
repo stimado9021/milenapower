02f9a6d814593287b51e7adc3dc29cd9b4f76be8
stimado9021 <quinteroenrrique321@gmail.com>
2024-06-08 12:47:15 -0500

    mlena power sistema de inventario deploy en vercel al fin

--- index.js
+++ index.js
@@ -1,58 +1,60 @@
-const express = require('express');
-const mongoose  = require('mongoose');
-const bodyParser = require('body-parser');
-const session = require('express-session');
-const morgan = require('morgan')
-const Swal = require('sweetalert2')
-require('dotenv').config()
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const morgan = require("morgan");
+const Swal = require("sweetalert2");
+require("dotenv").config();
 //const MongoStore = require('connect-mongo');
-const acceso = require('./midleware/acceso');
-const cors = require('cors');
+const acceso = require("./midleware/acceso");
+const cors = require("cors");
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(bodyParser.json())
-app.use(cors({
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(
+  cors({
+   // origin: "Localhost:3000",
     origin:'https://milenapower.vercel.app/',
-    credentials:true
-}))
+    credentials: true,
+  })
+);
 
-app.set('view engine', "ejs");
-app.set('views', __dirname+ '/views');
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
 app.use(express.static(__dirname + "/public"));
-app.use(express.json())
-app.use(morgan())
+app.use(express.json());
+app.use(morgan());
 
-
-const port =process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 // const user = process.env.USERDATABASE
 // const password= process.env.PASSWORDDATABASE;
 // const database = process.env.DATABASE;
-const uri=process.env.URI
+const uri = process.env.URI;
 
-mongoose.connect(uri,{useNewUrlParser:true,useUnifiedTopology:true})
-.then(()=>console.log('base de datos conectada'))
-.catch(e=>console.log(e));
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("base de datos conectada"))
+  .catch((e) => console.log(e));
 
-app.use(session({
-    secret:process.env.SECRET,
-    resave:false,
-    saveUninitialized:false,
-    cookie:{
-        secure:process.env.ENVIRONMENT === 'production' ? "true" : "auto",
-        httpOnly:true,
-        expires:1000*60*60*24*7,
-        sameSite:process.env.ENVIRONMENT === 'production' ? "true" : "lax"
-    }
-   
-}))
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: process.env.ENVIRONMENT === "production" ? "true" : "auto",
+      httpOnly: true,
+      expires: 1000 * 60 * 60 * 24 * 7,
+      sameSite: process.env.ENVIRONMENT === "production" ? "true" : "lax",
+    },
+  })
+);
 
-app.use('/', require('./router/rutasweb'))
-app.use('/usuario' ,require('./router/usuarioRouter'))
-app.use('/product', require('./router/productRouter'))
+app.use("/", require("./router/rutasweb"));
+app.use("/usuario", require("./router/usuarioRouter"));
+app.use("/product", require("./router/productRouter"));
 
-
-app.listen(port,()=>{
-    console.log('corriendo en el puerto ' + port);
-})
-
+app.listen(port, () => {
+  console.log("corriendo en el puerto " + port);
+});
--- router/usuarioRouter.js
+++ router/usuarioRouter.js
@@ -1,40 +1,40 @@
-const express = require('express')
-const router = express.Router()
-const session = require('express-session')
-const Usuario = require('../models/usuarioModel')
+const express = require("express");
+const router = express.Router();
+const session = require("express-session");
+const Usuario = require("../models/usuarioModel");
 
-
-router.get('/',(req,res)=>{
-   console.log(req.session.users)
-   res.render('dashboard',{users:req.session.users,isLogin: req.session.isLogin})
-})
-
-router.get('/profile',(req,res)=>{
-   res.render('profile')
-})
-
-router.post('/',async(req,res)=>{
- const {usuario,password} = req.body;
- 
- const user = await Usuario.find({usuario:usuario}).exec();
- const logger = user[0];
- 
- if(!logger){  
-    res.render('error404',{message:' Usuario No Esta En Base de Datos '})
- }
-
-if(logger.password !== password){
-   
-    res.render('error404',{message:' falla al autentificar usuario '})
-} 
-
-req.session.isLogin=true;
-req.session.users=usuario;  
-    
-res.redirect('usuario')
-                
+//ruta hacia el dashboard despuies de loguearse
+router.get("/", (req, res) => {
+  res.render("dashboard", {
+    users: req.session.users,
+    isLogin: req.session.isLogin,
+  });
+});
+//ruta hacia elprofile en contruccion todavia
+router.get("/profile", (req, res) => {
+  res.render("profile");
 });
 
+//ruta para loguearse
+router.post("/", async (req, res) => {
+  const { usuario, password } = req.body;
 
+  const user = await Usuario.find({ usuario: usuario.toLowerCase() }).exec();
+  const logger = user[0];
 
-module.exports = router
+  if (!logger) {
+    res.render("error404", { message: " Usuario No Esta En Base de Datos " });
+  }else{
+   if (logger.password !== password.toLowerCase()) {
+    res.render("error404", { message: " falla al autentificar usuario " });
+  } else {
+    req.session.isLogin = true;
+    req.session.users = usuario;
+    res.redirect("usuario");
+  }
+  }
+
+  
+});
+
+module.exports = router;
